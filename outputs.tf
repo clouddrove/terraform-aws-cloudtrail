2ff27020bd7c3b3c86e2d6f0f13edb5eb3cb7964
Nikita Dugar <nikita@clouddrove.com>
2019-09-24 12:55:30 +0530

    change output syntax

--- outputs.tf
+++ outputs.tf
@@ -2,23 +2,17 @@
 #Description : Terraform module to provision an AWS CloudTrail with encrypted S3 bucket.
 #              This bucket is used to store CloudTrail logs.
 output "id" {
-  value = concat(
-    aws_cloudtrail.default.*.id
-  )[0]
+  value = join("", aws_cloudtrail.default.*.id)
   description = "The name of the trail."
 }
 
 output "home_region" {
-  value = concat(
-    aws_cloudtrail.default.*.home_region
-  )[0]
+  value = join("", aws_cloudtrail.default.*.home_region)
   description = "The region in which the trail was created."
 }
 
 output "arn" {
-  value = concat(
-    aws_cloudtrail.default.*.arn
-  )[0]
+  value = join("", aws_cloudtrail.default.*.arn)
   description = "The Amazon Resource Name of the trail."
 }
 
