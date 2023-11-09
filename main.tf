4c692814d16d40a1829dd5db1fdd28da34def1e1
Himanshu Ahirwar <83774016+h1manshu98@users.noreply.github.com>
2023-11-09 21:41:39 +0530

    feat: s3_key_prefix attribute for cloudtrail (#26)

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ resource "aws_cloudtrail" "default" {
   name                          = module.labels.id
   enable_logging                = var.enable_logging
   s3_bucket_name                = var.s3_bucket_name
+  s3_key_prefix                 = var.s3_key_prefix
   enable_log_file_validation    = var.enable_log_file_validation
   is_multi_region_trail         = var.is_multi_region_trail
   include_global_service_events = var.include_global_service_events
